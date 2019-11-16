d2d20bfa94b68147f2ea1a5180899cf835764c9d
MatthewCalligaro <matthewcalligaro@hotmail.com>
2019-11-16 13:39:43 -0800

    Protect against invalid moves from agents

--- game.cpp
+++ game.cpp
@@ -1,9 +1,11 @@
 // Copyright 2019 Matthew Calligaro
 
 #include <memory>
+#include <iostream>
 #include <ostream>
 #include "game.hpp"
 
+using std::cerr;
 using std::endl;
 using std::ostream;
 using std::shared_ptr;
@@ -13,14 +15,21 @@ Game::Game(shared_ptr<Agent> xAgent, shared_ptr<Agent> oAgent) {
     agents_[1] = oAgent;
 }
 
-// TODO(MatthewCalligaro): Protect against bad moves
 // TODO(MatthewCalligaro): Add time limit
 size_t Game::execute() {
     size_t moves = 0;
 
     // Allow each agent to play on their turn until the game is won or a draw
     while (moves < 42 && !board_.isWon()) {
-        board_.handleMove(agents_[board_.getTurn()]->getMove(board_));
+        size_t move = agents_[board_.getTurn()]->getMove(board_);
+        if (!board_.isValidMove(move)) {
+            size_t badMove = move;
+            move = board_.getSuccessors()[0];
+            cerr << agents_[board_.getTurn()]->getAgentName() << " made an " << 
+                "invalid move (" << badMove << "). Using move " << move << 
+                " instead."; 
+        }
+        board_.handleMove(move);
         ++moves;
     }
 
