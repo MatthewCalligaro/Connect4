0bec50f9ed370f09ab9b1b968db777156ccba3ac
MatthewCalligaro <MatthewCalligaro@hotmail.com>
2019-11-22 02:24:53 -0800

    working on time limit

--- game.cpp
+++ game.cpp
@@ -1,16 +1,23 @@
 // Copyright 2019 Matthew Calligaro
 
-#include <memory>
+#include <chrono>
+#include <future>
 #include <iostream>
+#include <memory>
 #include <ostream>
+#include <thread>
 #include "game.hpp"
 
 using std::cerr;
 using std::endl;
+using std::future;
 using std::ostream;
+using std::promise;
 using std::shared_ptr;
+using std::thread;
 
-Game::Game(shared_ptr<Agent> xAgent, shared_ptr<Agent> oAgent) {
+Game::Game(shared_ptr<Agent> xAgent, shared_ptr<Agent> oAgent, 
+    size_t turnTime) : turnTime_{turnTime} {
     agents_[0] = xAgent;
     agents_[1] = oAgent;
 }
@@ -21,7 +28,7 @@ size_t Game::execute() {
 
     // Allow each agent to play on their turn until the game is won or a draw
     while (moves < 42 && !board_.isWon()) {
-        size_t move = agents_[board_.getTurn()]->getMove(board_);
+        size_t move = getMove(board_.getTurn());
         if (!board_.isValidMove(move)) {
             size_t badMove = move;
             move = board_.getSuccessors()[0];
@@ -43,4 +50,25 @@ size_t Game::execute() {
 ostream& Game::printBoard(ostream& os) const {
     os << board_;
     return os;
+}
+
+size_t Game::getMove(size_t agent) {
+    promise<void> timeout;
+    size_t move = 7;
+
+    // Create thread 
+    thread getMoveThread(getMoveThreadFunction, move, agent, 
+        timeout.get_future());
+
+    // Wait for turn time
+    std::this_thread::sleep_for(std::chrono::milliseconds(turnTime_));
+    timeout.set_value();
+    thread.join();
+
+    return move;
+}
+
+void Game::getMoveThreadFunction(size_t& move, size_t agent, 
+    future<void>& timeout) {
+    
 }
--- game.hpp
+++ game.hpp
@@ -3,11 +3,13 @@
 #ifndef GAME_HPP_
 #define GAME_HPP_
 
+#include <future>
 #include <memory>
 #include <ostream>
 #include "agent.hpp"
 #include "board.hpp"
 
+using std::future;
 using std::shared_ptr;
 using std::ostream;
 
@@ -15,7 +17,8 @@ class Game {
  public:
     Game() = delete;
     Game(const Game& other) = default;
-    Game(shared_ptr<Agent> xAgent, shared_ptr<Agent> oAgent);
+    Game(shared_ptr<Agent> xAgent, shared_ptr<Agent> oAgent, 
+        size_t turnTime = 2000);
     ~Game() = default;
     Game& operator=(const Game& other) = default;
 
@@ -25,6 +28,10 @@ class Game {
  private:
     Board board_;
     shared_ptr<Agent> agents_[2];
+    size_t turnTime_;
+
+    size_t getMove(size_t agent);
+    size_t getMoveThreaded(size_t agent, future<void> timeout);
 };
 
 #endif  // GAME_HPP_
