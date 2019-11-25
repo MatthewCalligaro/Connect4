bdcf9a02870ea237f61b57e89f2831f3a0dc2849
MatthewCalligaro <MatthewCalligaro@hotmail.com>
2019-11-25 02:50:34 -0800

    Add multiple trial testing

--- c4.cpp
+++ c4.cpp
@@ -1,43 +1,103 @@
 // Copyright 2019 Matthew Calligaro
 
+#include <fstream>
 #include <iostream>
 #include <memory>
+#include <string>
 #include "agent-human.hpp"
 #include "agent-minimax.hpp"
 #include "agent-null.hpp"
 #include "game.hpp"
 
-using std::cout;
-using std::endl;
-using std::make_shared;
-using std::shared_ptr;
-
 void singleGame() {
-  shared_ptr<Agent> a1 = make_shared<AgentNull>();
-  shared_ptr<Agent> a2 = make_shared<AgentMinimax>();
-  Game game(a1, a2, 2000);
+  std::shared_ptr<Agent> ax = std::make_shared<AgentMinimax>(4, 0);
+  std::shared_ptr<Agent> ao = std::make_shared<AgentMinimax>(12, 0.01);
+  Game game(ax, ao, 2000);
 
-  cout << a1->getAgentName() << " vs. " << a2->getAgentName() << endl;
+  std::cout << ax->getAgentName() << " vs. " << ao->getAgentName() << std::endl;
   size_t winner = game.execute(true);
 
-  cout << "----------------------------------------------------------------"
-       << endl
-       << "Final Board:" << endl;
-  game.printBoard(cout);
+  std::cout
+      << "----------------------------------------------------------------"
+      << std::endl
+      << "Final Board:" << std::endl;
+  game.printBoard(std::cout);
 
   switch (winner) {
     case 0:
-      cout << a1->getAgentName() << " (X Player) won" << endl;
+      std::cout << ax->getAgentName() << " (X Player) won" << std::endl;
       break;
     case 1:
-      cout << a2->getAgentName() << " (O Player) won" << endl;
+      std::cout << ao->getAgentName() << " (O Player) won" << std::endl;
       break;
     case 2:
-      cout << "Draw" << endl;
+      std::cout << "Draw" << std::endl;
       break;
   }
 }
 
+void trials() {
+  const size_t NUM_TRIALS = 10;
+  const size_t DEPTH = 9;
+  const size_t TIME_LIMIT = 10000;
+  std::ofstream file("data/9deep-Memo.csv");
+
+  std::shared_ptr<Agent> ax;
+  std::shared_ptr<Agent> ao;
+
+  std::array<double, 42> trials[NUM_TRIALS * 2];
+  for (size_t i = 0; i < NUM_TRIALS * 2; ++i) {
+    for (double& move : trials[i]) {
+      move = 0;
+    }
+  }
+
+  // Execute games
+  for (size_t i = 0; i < NUM_TRIALS; ++i) {
+    ax = std::make_shared<AgentMinimax>(4, 0);
+    ao = std::make_shared<AgentMinimax>(DEPTH, 0.01);
+
+    Game game(ax, ao, TIME_LIMIT);
+    size_t winner = game.execute(trials[i], trials[i + NUM_TRIALS]);
+
+    std::cout << "Trial " << i + 1 << ": ";
+    switch (winner) {
+      case 0:
+        std::cout << ax->getAgentName() << " (X Player) won" << std::endl;
+        break;
+      case 1:
+        std::cout << ao->getAgentName() << " (O Player) won" << std::endl;
+        break;
+      case 2:
+        std::cout << "Draw" << std::endl;
+        break;
+    }
+  }
+
+  // Create CSV header
+  file << ax->getAgentName() << " Trial 1,";
+  for (size_t i = 2; i <= NUM_TRIALS; ++i) {
+    file << "Trial " << i << ",";
+  }
+  file << ao->getAgentName() << " Trial 1,";
+  for (size_t i = 2; i <= NUM_TRIALS; ++i) {
+    file << "Trial " << i << ",";
+  }
+  file << std::endl;
+
+  // Fill CSV with data
+  for (size_t r = 0; r < 42; ++r) {
+    for (size_t trial = 0; trial < NUM_TRIALS * 2; ++trial) {
+      if (trials[trial][r] > 0) {
+        file << trials[trial][r];
+      }
+      file << ",";
+    }
+    file << std::endl;
+  }
+}
+
 int main() {
-  singleGame();
+  // singleGame();
+  trials();
 }
